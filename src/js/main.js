69b62050a3f3754acb32ff0290b8061ba341c69c
yoichi kobayashi <info@tplh.net>
2016-01-22 20:40:29 +0900

    [modify] ダミーオブジェクトの数を実際のオブジェクト数に近いものに更新。

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ import resizeWindow from './modules/resizeWindow.js';
 const glslify = require('glslify');
 const canvas = document.getElementById('webgl-contents');
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
+const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 100000);
 const renderer = new THREE.WebGLRenderer({
   antialias: true
 });
@@ -13,17 +13,24 @@ const light = new THREE.HemisphereLight(0xffffff, 0x333333, 1);
 const init = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
-  //resizeWindow(canvas);
 
-  camera.position.set(100, 100, 100);
+  camera.position.set(5000, 5000, 5000);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
-  const geometry = new THREE.BoxGeometry(100, 100, 100);
+  const geometry = new THREE.PlaneGeometry(100, 100, 2, 2);
   const material = new THREE.MeshBasicMaterial({
     color: 0xffffff
   });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  for (var i = 0; i < 500; i++) {
+    const cube = new THREE.Mesh(geometry, material);
+    cube.position.copy(Util.getPolar(
+      Util.getRadian(Util.getRandomInt(0, 359)),
+      Util.getRadian(Util.getRandomInt(0, 359)),
+      3000
+    ));
+    scene.add(cube);
+  }
+
   scene.add(light);
 
   renderLoop();
