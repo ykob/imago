c118d6f874e013aa6a6be90cc22c7bc68ad1f73d
yoichi kobayashi <info@tplh.net>
2016-01-23 23:14:04 +0900

    [add] 各オブジェクトを中心点に向け、中心からの距離をランダムにする。

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,7 @@ import resizeWindow from './modules/resizeWindow.js';
 const glslify = require('glslify');
 const canvas = document.getElementById('webgl-contents');
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 100000);
+const camera = new THREE.PerspectiveCamera(35, window.innerWidth / window.innerHeight, 1, 100000);
 const renderer = new THREE.WebGLRenderer({
   antialias: true
 });
@@ -14,20 +14,23 @@ const init = () => {
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  camera.position.set(5000, 5000, 5000);
+  camera.position.set(7000, 7000, 5000);
   camera.lookAt(new THREE.Vector3(0, 0, 0));
 
+  const center_point = new THREE.Vector3();
   const geometry = new THREE.PlaneGeometry(100, 100, 2, 2);
-  const material = new THREE.MeshBasicMaterial({
-    color: 0xffffff
+  const material = new THREE.MeshPhongMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide
   });
   for (var i = 0; i < 500; i++) {
     const cube = new THREE.Mesh(geometry, material);
     cube.position.copy(Util.getPolar(
+      Util.getRadian(Util.getRandomInt(30, 150)),
       Util.getRadian(Util.getRandomInt(0, 359)),
-      Util.getRadian(Util.getRandomInt(0, 359)),
-      3000
+      Util.getRandomInt(1500, 3000)
     ));
+    cube.lookAt(center_point);
     scene.add(cube);
   }
 
